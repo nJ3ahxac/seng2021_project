2f4492e7fca15efbdd21df426204eb15f7fc352f
Nicolas Salvestro-Martin <z5319842@ad.unsw.edu.au>
2021-02-27 13:11:42 +1100

    Add class MovieData template

---
+++ SourceCode_and_Documentation/src/client/client.hh
@@ -0,0 +1,37 @@
+#ifndef CLIENT_HH_
+#define CLIENT_HH_
+
+// Library for parsing json; github.com/nlohmann/json
+#include "json/json.hh"
+using nlohmann::json;
+
+// Library for making requests.
+#include <curl/curl.h>
+
+#include <filesystem>
+#include <fstream>
+#include <string>
+
+class MovieData {
+private:
+public:
+    class cache_error : public std::exception {
+    public:
+        virtual const char* what() const noexcept override { 
+            return "Bad cache read";
+        }
+    };
+
+    enum class construct { with_cache, without_cache };
+
+    // The constructor may be supplied with with or without cache.
+    // with_cache loads the data from the local dir "cache", and throws a
+    // cache_error if it fails to parse. Without cache regenerates the cache
+    // and overwrites the prexisting one.
+    // with_cache is preferred over without_cache as it is far slower. It
+    // should only be performed between long intervals when updating the movie
+    // data is necessary.
+    MovieData(const construct&);
+};
+
+#endif
