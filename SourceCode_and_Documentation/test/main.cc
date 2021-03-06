cfbc4ba4003886dee0c974105595e89c2e7349d5
Nicolas Salvestro-Martin <z5319842@ad.unsw.edu.au>
2021-03-06 19:59:44 +1100

    Add frontend overview page, debug test failure

--- SourceCode_and_Documentation/test/main.cc
+++ SourceCode_and_Documentation/test/main.cc
@@ -1,6 +1,24 @@
 #include "main.hh"
 
+// Sets the file limit, if 0 sets to max available.
+[[maybe_unused]] static void set_file_limit(const int n) {
+    const auto read_limits = []() {
+        rlimit ret;
+        getrlimit(RLIMIT_NOFILE, &ret);
+        return ret;
+    };
+    const auto set_limits = [&](const rlimit& r) {
+        setrlimit(RLIMIT_NOFILE, &r);
+    };
+    auto limits = read_limits();
+    std::cout << "old limit: " << limits.rlim_cur << '\n';
+    limits.rlim_cur = n == 0 ? limits.rlim_max : n;
+    set_limits(limits);
+    std::cout << "new limit: " << limits.rlim_cur << '\n';
+}
+
 int main(int argc, char* argv[]) {
+    //set_file_limit(0);
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
--- SourceCode_and_Documentation/test/main.hh
+++ SourceCode_and_Documentation/test/main.hh
@@ -1,6 +1,9 @@
 #ifndef TEST_MAIN_HH_
 #define TEST_MAIN_HH_
 
+#include <sys/resource.h>
+#include <sys/time.h>
+
 #include "gtest/gtest.h"
 
 #endif
