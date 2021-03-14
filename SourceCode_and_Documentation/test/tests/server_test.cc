36cce1e5df79b99a976ccc70bd6772093b4a40c5
Nicolas Salvestro-Martin <z5319842@ad.unsw.edu.au>
2021-03-14 23:09:11 +1100

    Add util::request class, refactor tests

--- SourceCode_and_Documentation/test/tests/server_test.cc
+++ SourceCode_and_Documentation/test/tests/server_test.cc
@@ -6,76 +6,26 @@ TEST_F(ServerTest, visible_web_directory) {
     PageHandler{};
 }
 
-// Returns http code.
-static long ping_url(const std::string& url, const int port) {
-    CURL* const curl = curl_easy_init();
-    if (!curl) {
-        throw std::runtime_error("Failed to get curl handle for request");
-    }
-
-    // If we don't set this, it prints to cout.
-    const auto write_callback = [](char* contents, std::size_t size,
-                                   std::size_t nmemb,
-                                   void* userp) { return size * nmemb; };
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-    curl_easy_setopt(curl, CURLOPT_PORT, port);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, *write_callback);
-    CURLcode result = curl_easy_perform(curl);
-    curl_easy_cleanup(curl);
-
-    if (result != CURLE_OK) {
-        throw std::runtime_error(std::string("Request failed: ") +
-                                 curl_easy_strerror(result));
-    }
-
-    long http_code = 0;
-    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
-    return http_code;
-}
-
 TEST_F(ServerTest, bindings_code_200) {
     PageHandler handler;
     for (const auto& b : handler.get_bindings()) {
-        EXPECT_EQ(ping_url("localhost" + b.first, test_port), 200L);
+        const auto url = "localhost" + b.first;
+        EXPECT_EQ(util::request(url, test_port).get_code(), 200L);
     }
 }
 
 TEST_F(ServerTest, resources_code_200) {
     PageHandler handler;
     for (const auto& r : handler.get_resources()) {
-        EXPECT_EQ(ping_url("localhost" + r.first, test_port), 200L);
+        const auto url = "localhost" + r.first;
+        EXPECT_EQ(util::request(url, test_port).get_code(), 200L);
     }
 }
 
 TEST_F(ServerTest, not_existent_code_404) {
     PageHandler handler;
-    EXPECT_EQ(ping_url("localhost/_____DOES_NOT_EXIST.wav", test_port), 404L);
-}
-
-static std::string download_url_string(const std::string& url, const int port) {
-    CURL* const curl = curl_easy_init();
-    if (!curl) {
-        throw std::runtime_error("Unable to get curl handle for request");
-    }
-
-    std::string request_contents;
-    const auto write_callback = [](char* contents, std::size_t size,
-                                   std::size_t nmemb, std::string* userp) {
-        userp->append(contents, size * nmemb);
-        return size * nmemb;
-    };
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-    curl_easy_setopt(curl, CURLOPT_PORT, port);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, *write_callback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &request_contents);
-    CURLcode result = curl_easy_perform(curl);
-    curl_easy_cleanup(curl);
-
-    if (result != CURLE_OK) {
-        throw std::runtime_error(std::string("Request failed: ") +
-                                 curl_easy_strerror(result));
-    }
-    return request_contents;
+    const auto url = "localhost/_____DOES_NOT_EXIST.wav";
+    EXPECT_EQ(util::request(url, test_port).get_code(), 404L);
 }
 
 TEST_F(ServerTest, bindings_same_file) {
@@ -89,7 +39,8 @@ TEST_F(ServerTest, bindings_same_file) {
         std::copy(std::istreambuf_iterator<char>(input),
                   std::istreambuf_iterator<char>(),
                   std::inserter(file, file.begin()));
-        EXPECT_EQ(file, download_url_string("localhost" + b.first, test_port));
+        const auto url = "localhost" + b.first;
+        EXPECT_EQ(file, util::request(url, test_port).get_contents());
     }
 }
 
@@ -104,6 +55,7 @@ TEST_F(ServerTest, resources_same_file) {
         std::copy(std::istreambuf_iterator<char>(input),
                   std::istreambuf_iterator<char>(),
                   std::inserter(file, file.begin()));
-        EXPECT_EQ(file, download_url_string("localhost" + r.first, test_port));
+        const auto url = "localhost" + r.first;
+        EXPECT_EQ(file, util::request(url, test_port).get_contents());
     }
 }
--- SourceCode_and_Documentation/test/tests/server_test.hh
+++ SourceCode_and_Documentation/test/tests/server_test.hh
@@ -15,9 +15,10 @@
 #include <pistache/endpoint.h>
 
 #include "main.hh"
-#include "server.hh"
+#include "server/server.hh"
+#include "util/util.hh"
 
-static int test_port = PORT_DEFAULT; // avoid same port bind at runtime
+static int test_port = PORT_DEFAULT;
 
 // Since googletest inits a new fixtures each test, we will have to work around
 // failing to bind to the same port within short periods by incrementing
@@ -29,7 +30,7 @@ private:
 protected:
     ServerTest() {
         // Blocking until the server starts.
-        const auto start_server = [&, this]() {
+        const auto start_server = []() {
             const Pistache::Address addr(Pistache::Ipv4::any(),
                                          Pistache::Port(++test_port));
             const auto opts =
@@ -45,30 +46,8 @@ protected:
     // To avoid a race condition, we must wait for the server to init on the
     // other thread before running any tests.
     void SetUp() override {
-        const auto has_responded = [](const std::string& url,
-                                      const int port) -> bool {
-            CURL* const curl = curl_easy_init();
-            if (!curl) {
-                return false;
-            }
-            const auto write_callback = [](char* contents, std::size_t size,
-                                           std::size_t nmemb, void* userp) {
-                return size * nmemb;
-            };
-            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-            curl_easy_setopt(curl, CURLOPT_PORT, port);
-            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, *write_callback);
-            CURLcode result = curl_easy_perform(curl);
-            curl_easy_cleanup(curl);
-
-            if (result != CURLE_OK) {
-                return false;
-            }
-            return true;
-        };
-
         const auto start = std::chrono::steady_clock::now();
-        while (!has_responded("localhost", test_port)) {
+        while (util::request("localhost", test_port).get_code() != 200) {
             const auto now = std::chrono::steady_clock::now();
             if (now > start + std::chrono::seconds(1)) {
                 throw std::runtime_error("Fixture server did not respond");
