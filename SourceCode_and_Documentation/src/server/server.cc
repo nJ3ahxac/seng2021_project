41183b770597b8464acbd2a23cdd5517fb20f9b2
ben5s <ben5@tuta.io>
2021-03-17 13:20:56 +1100

    restructure server.cc for pending get request support

--- SourceCode_and_Documentation/src/server/server.cc
+++ SourceCode_and_Documentation/src/server/server.cc
@@ -1,10 +1,7 @@
 #include "server.hh"
 
-PageHandler::PageHandler()
-    : Pistache::Http::Handler(), bindings{{"/", "/main.html"},
-                                          {"/search", "/search.html"},
-                                          {"/about", "/about.html"}},
-      resources() {
+static std::unordered_map<std::string, std::string> create_resources() {
+    std::unordered_map<std::string, std::string> resources;
     // Put new files in files, put new bindings in bindings!
     const std::vector<std::string> files = {"/404.html",
                                             "/main.html",
@@ -26,52 +23,55 @@ PageHandler::PageHandler()
         const std::string file_contents = util::read_file("web" + filename);
         resources.insert({filename, file_contents});
     }
+    return resources;
+}
+
+PageHandler::PageHandler()
+    : Pistache::Http::Handler(), bindings{{"/", "/main.html"},
+                                          {"/search", "/search.html"},
+                                          {"/about", "/about.html"}},
+        resources(create_resources()),
+        tokens(max_token_storage) {
 };
 
 void PageHandler::handle_post_request(
-    const Pistache::Http::Request& request,
-    Pistache::Http::ResponseWriter& response) {
-    // TODO: Handle post request here
+        const Pistache::Http::Request& request,
+        Pistache::Http::ResponseWriter& response) {
+    
 }
 
-void PageHandler::onRequest(const Pistache::Http::Request& request,
-                            Pistache::Http::ResponseWriter response) {
-    // std::cout << "Got request with resource: " << request.resource() <<
-    // '\n';
-    const auto method = request.method();
-    if (method != Pistache::Http::Method::Post &&
-        method != Pistache::Http::Method::Get) {
-        return;
-    }
-
-    if (method == Pistache::Http::Method::Post) {
-        try {
-            handle_post_request(request, response);
-        } catch (const std::exception& e) {
-            // Return a simple json error message if we can't parse the post.
-            std::string resp = "{error: " + std::string(e.what()) + "}";
-            response.send(Pistache::Http::Code::Bad_Request, resp);
-        }
-        return;
-    }
-
+void PageHandler::handle_get_request(
+        const Pistache::Http::Request& request,
+        Pistache::Http::ResponseWriter& response) {
     // There are three cases for providing files here.
     // 1. The page requested is in our bindings, eg "/" -> "main.html"
     if (const auto it = bindings.find(request.resource());
         it != bindings.end()) {
-
-        response.send(Pistache::Http::Code::Ok, resources[it->second]);
+        response.send(Pistache::Http::Code::Ok, resources.at(it->second));
         return;
     }
 
     // 2. The page requested is in our std::map of files (usually .js)
     if (const auto it = resources.find(request.resource());
         it != resources.end()) {
-
         response.send(Pistache::Http::Code::Ok, it->second);
         return;
     }
 
     // 3. The page does not exist, in which case we return 404.
-    response.send(Pistache::Http::Code::Not_Found, resources["/404.html"]);
+    response.send(Pistache::Http::Code::Not_Found, resources.at("/404.html"));
+}
+
+void PageHandler::onRequest(const Pistache::Http::Request& request,
+                            Pistache::Http::ResponseWriter response) {
+    switch (request.method()) {
+    case Pistache::Http::Method::Post:
+        handle_post_request(request, response);
+        break;
+    case Pistache::Http::Method::Get:
+        handle_get_request(request, response);
+        break;
+    default:
+        break;
+    }
 }
--- SourceCode_and_Documentation/src/server/server.hh
+++ SourceCode_and_Documentation/src/server/server.hh
@@ -1,25 +1,30 @@
 #ifndef SERVER_SERVER_HH_
 #define SERVER_SERVER_HH_
 
+#include "search.hh"
+#include "util/util.hh"
+
+#include <pistache/endpoint.h>
+#include <boost/circular_buffer.hpp>
+
 #include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <iterator>
-#include <map>
+#include <unordered_map>
 #include <string>
 
-#include <pistache/endpoint.h>
-
-#include "util/util.hh"
 
 class PageHandler : public Pistache::Http::Handler {
 private:
+    static constexpr int max_token_storage = 100u;
     // This structure maps a binding to a filename, ie, "/" -> "/main.html"
     // Basically associations of web directory to html files.
-    std::map<std::string, std::string> bindings;
+    const std::unordered_map<std::string, std::string> bindings;
     // Map of all files, where the key is their directory.
-    std::map<std::string, std::string> resources;
-
+    const std::unordered_map<std::string, std::string> resources;
+    // Circular buffer of search tokens
+    boost::circular_buffer<search::token> tokens;
 public:
     // Caches all web files provided and associates bindings for later lookup.
     // Throws std::runtime_error if a file does not exist, hopefully before the
@@ -33,15 +38,13 @@ public:
     void onRequest(const Pistache::Http::Request& request,
                    Pistache::Http::ResponseWriter response) override;
     // Handles post requests, if an exception was thrown, the response is bad
+    const auto& get_bindings() const noexcept { return bindings; }
+    const auto& get_resources() const noexcept { return resources; }
+private:
     void handle_post_request(const Pistache::Http::Request& request,
                              Pistache::Http::ResponseWriter& response);
-
-    const std::map<std::string, std::string>& get_bindings() const noexcept {
-        return bindings;
-    }
-    const std::map<std::string, std::string>& get_resources() const noexcept {
-        return resources;
-    }
+    void handle_get_request(const Pistache::Http::Request& request,
+                             Pistache::Http::ResponseWriter& response);
 };
 
 #endif
